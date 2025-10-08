3479758d3eeea4a839b0831c779213391990efd2
171neha <nehaj6983@gmail.com>
2025-10-08 17:27:07 +0530

    Hotel roon booking content added

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import Structure from "@/components/common/page-structure";
 import Header from "@/components/section-heading";
 import Section from "@/components/section-structure";
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 
 // Images
 import IOTImg from "@/assests/portfolio/IOT EV Charging Platform.jpeg";
@@ -15,22 +15,22 @@ import Cab_Advertisement_App from "@/assests/portfolio/Cab Advertisement App.jpe
 import Lab_Report from "@/assests/portfolio/Lab Report Management-1.png";
 import Droshipping_portal from "@/assests/portfolio/Dropshipping Portal.webp";
 import Droshipping_platform from "@/assests/portfolio/Dropshipping Platform.jpeg";
-import smart_Delivery from "@/assests/portfolio/Smart Delivery Admin System.png";
 import smart_Delivery_admin from "@/assests/portfolio/Delivery.png";
-import smart_Delivery_Distributor from "@/assests/portfolio/Delivery.png";
+import Hotel_Room_Booking from "@/assests/portfolio/Room-1.png";
 
 
 const projects = [
-  { id: "iot-ev-charging", title: "IOT EV Charging Platform", category: "Mobile", image: IOTImg },
-  { id: "foodcourt-admin", title: "Foodcourt Admin", category: "Web", image: Foodcourt_Admin },
-  { id: "food-court-app", title: "Food Court App", category: "Mobile", image: Foodcourt_App },
-  { id: "cab-advertisement-app", title: "Cab Advertisement App", category: "Mobile", image: Cab_Advertisement_App },
-  { id: "lab-report-management", title: "Lab Report Management", category: "Web", image: Lab_Report },
-  { id: "dropshipping-portal", title: "Dropshipping Portal", category: "Web", image: Droshipping_portal },
-  { id: "dropshipping-platform", title: "Dropshipping Platform", category: "Web", image: Droshipping_platform },
-  { id: "smart-delivery-admin", title: "Grocery Delivery", category: "Web", image: smart_Delivery_admin },
-  //{ id: "smart-delivery-admin", title: "Smart Delivery Admin System", category: "Web", image: smart_Delivery },
-  // { id: "smart-delivery-distributor", title: "Grocery Delivery Distributor", category: "Mobile", image: smart_Delivery_Distributor },
+
+  {id: "smart-delivery-admin", title: "Grocery Delivery", category: "Web", image: smart_Delivery_admin},
+  {id: "hotel-room-booking", title: "Hotel Room Booking", category: "Web", image: Hotel_Room_Booking},
+  {id: "lab-report-management", title: "Lab Report Management", category: "Web", image: Lab_Report},
+  {id: "iot-ev-charging", title: "IOT EV Charging Platform", category: "Mobile", image: IOTImg},
+  {id: "foodcourt-admin", title: "Foodcourt Admin", category: "Web", image: Foodcourt_Admin},
+  {id: "food-court-app", title: "Food Court App", category: "Mobile", image: Foodcourt_App},
+  {id: "cab-advertisement-app", title: "Cab Advertisement App", category: "Mobile", image: Cab_Advertisement_App},
+  {id: "dropshipping-portal", title: "Dropshipping Portal", category: "Web", image: Droshipping_portal},
+  {id: "dropshipping-platform", title: "Dropshipping Platform", category: "Web", image: Droshipping_platform},
+
 ];
 
 const categories = ["All", "Mobile", "Web", "Game", "AR/VR", "Blockchain", "UI/UX Design", ".NET"];
@@ -58,11 +58,10 @@ const Page = () => {
             <Button
               key={cat}
               onClick={() => setActiveCategory(cat)}
-              className={`rounded-full px-6 py-2 transition-transform transform hover:scale-110 ${
-                activeCategory === cat
-                  ? "bg-sky-500 text-white shadow-lg"
-                  : "bg-gray-200 text-gray-800 hover:bg-gray-300"
-              }`}
+              className={`rounded-full px-6 py-2 transition-transform transform hover:scale-110 ${activeCategory === cat
+                ? "bg-sky-500 text-white shadow-lg"
+                : "bg-gray-200 text-gray-800 hover:bg-gray-300"
+                }`}
             >
               {cat}
             </Button>
